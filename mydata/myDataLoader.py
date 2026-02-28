58f61290c0a9354337c52a15149fe092ad94ceba
Vineet Raj <vineetrajrj26@gmai.com>
2026-02-28 21:35:22 +0530

    fix-2

--- mydata/myDataLoader.py
+++ mydata/myDataLoader.py
@@ -5,15 +5,11 @@ from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
 
 
-class MSDataLoader(DataLoader):
+class MSDataLoader:
     """Multi-scale DataLoader compatible with modern PyTorch versions.
     
-    This loader handles multi-scale training by randomly selecting a scale
-    for each batch and appending the scale index to the batch data.
-    
-    Note: For multi-scale training (len(scale) > 1), num_workers is set to 0
-    to ensure scale changes are visible to the dataset during data loading.
-    For single-scale training, multi-threading works normally.
+    Uses composition instead of inheritance to avoid PyTorch 2.x 
+    DataLoader's __setattr__ restrictions.
     """
     
     def __init__(self, args, dataset, batch_size=1, shuffle=False,
@@ -21,53 +17,56 @@ class MSDataLoader(DataLoader):
                  collate_fn=default_collate, pin_memory=False, drop_last=False,
                  timeout=0, worker_init_fn=None):
         
-        scale = args.scale
+        self.scale = args.scale
+        self._dataset = dataset
         
         # For multi-scale training, we need single-threaded loading
-        # so that set_scale() is visible to __getitem__
         num_workers = args.n_threads
-        if len(scale) > 1 and hasattr(dataset, 'train') and dataset.train:
+        if len(self.scale) > 1 and hasattr(dataset, 'train') and dataset.train:
             num_workers = 0
         
-        super(MSDataLoader, self).__init__(
+        # Create internal DataLoader
+        self._loader = DataLoader(
             dataset, batch_size=batch_size, shuffle=shuffle,
             sampler=sampler, batch_sampler=batch_sampler,
             num_workers=num_workers, collate_fn=collate_fn,
             pin_memory=pin_memory, drop_last=drop_last,
             timeout=timeout, worker_init_fn=worker_init_fn)
-        
-        # Store custom attributes using object.__setattr__ to bypass DataLoader's restrictions
-        object.__setattr__(self, '_scale', scale)
-        object.__setattr__(self, '_ms_dataset', dataset)
 
     @property
-    def scale(self):
-        return self._scale
+    def dataset(self):
+        return self._dataset
+    
+    @property
+    def batch_sampler(self):
+        return self._loader.batch_sampler
+    
+    @property
+    def collate_fn(self):
+        return self._loader.collate_fn
+
+    def __len__(self):
+        return len(self._loader)
 
     def __iter__(self):
-        scale = self._scale
-        ms_dataset = self._ms_dataset
-        
         # For single-scale, just iterate normally and append scale index
-        if len(scale) == 1:
-            for batch in super().__iter__():
+        if len(self.scale) == 1:
+            for batch in self._loader:
                 if isinstance(batch, (list, tuple)):
                     yield list(batch) + [0]
                 else:
                     yield batch
         else:
             # For multi-scale, set scale before each batch
-            # This works because num_workers=0 for multi-scale
-            batch_sampler = self.batch_sampler
-            for batch_indices in batch_sampler:
+            for batch_indices in self._loader.batch_sampler:
                 idx_scale = 0
-                if hasattr(ms_dataset, 'train') and ms_dataset.train:
-                    idx_scale = random.randrange(0, len(scale))
-                    ms_dataset.set_scale(idx_scale)
+                if hasattr(self._dataset, 'train') and self._dataset.train:
+                    idx_scale = random.randrange(0, len(self.scale))
+                    self._dataset.set_scale(idx_scale)
                 
                 # Manually load and collate the batch
-                batch = [ms_dataset[i] for i in batch_indices]
-                batch = self.collate_fn(batch)
+                batch = [self._dataset[i] for i in batch_indices]
+                batch = self._loader.collate_fn(batch)
                 
                 if isinstance(batch, (list, tuple)):
                     yield list(batch) + [idx_scale]
